f87c81769bd0d8fb89807b516e7be8de0abe0257
James Pooley <james.pooley@childmind.org>
2016-06-12 21:44:11 -0400

    Add code to extract p-value

--- code/functions.R
+++ code/functions.R
@@ -1,12 +1,22 @@
 library(purrr)  # I use map all over the place; could also just use lapply from base
 
 
+# Helper function to get the age of peak cortical thickness for a single ROI
+# TODO: Still a lot of duplication going on here and don't like how the new data frame
+# construction is just happening out in the open
+
 construct_fmla <- function(roi, degree, motion_estimate = NULL) {
   fmla_string <- paste0(c(sprintf("%s ~ poly(age, degree = %s, raw = TRUE)", roi, degree),
                           motion_estimate),
                         collapse = "+")
   fmla <- as.formula(fmla_string)
   fmla
+}
+
+# Helper function to get the p-value of the regression coefficient estimate
+get_p_value <- function(fit_summary, nm) {
+  p <- fit_summary$coefficients[nm, 4]
+  p
 }
 
 
@@ -32,6 +42,11 @@ get_order <- function(p_vals, aic_vals) {
   model_order
 }
 
+# TODO: This is kind of pointless; combine with get_peak_age function?
+get_best_model <- function(model_fits) {
+  best_model <- compare_models(model_fits)
+  best_model
+}
 
 compare_models <- function(model_fits) {
   # TODO: Compare with base null model?
@@ -43,24 +58,14 @@ compare_models <- function(model_fits) {
                     "poly(age, degree = 2, raw = TRUE)2",
                     "poly(age, degree = 3, raw = TRUE)3")
   fit_summaries <- map(model_fits, summary)
-  p_vals <- map2(fit_summaries, age_coefs, summary)
-
+  p_vals <- map2(fit_summaries, age_coefs, get_p_value)
+  
   best_model <- get_order(p_vals, aic_vals)
   best_model
 }
 
 
-# TODO: This is kind of pointless; combine with get_peak_age function?
-get_best_model <- function(model_fits) {
-  best_model <- compare_models(model_fits)
-  best_model
-}
-
-
-# Helper function to get the age of peak cortical thickness for a single ROI
-# TODO: Still a lot of duplication going on here and don't like how the new data frame
-# construction is just happening out in the open
-get_peak_age <- function(roi, get_best_model, df, motion_estimate = NULL, ...) {
+get_peak_age <- function(roi, df, motion_estimate = NULL) {
   
   # Construct the model formulas for the given ROI
   degrees <- list(1, 2, 3)
