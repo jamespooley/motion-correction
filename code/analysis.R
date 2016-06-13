1d8b16974b49a8489838882f18830d0a7055ea32
James Pooley <james.pooley@childmind.org>
2016-06-13 11:32:51 -0400

    Add skeleton results data frame

--- code/analysis.R
+++ code/analysis.R
@@ -1,32 +1,9 @@
+library(readr)
+library(dplyr)
+library(purrr)
+library(ggplot2)
+
 source("code/functions.R")
-
-
-#devtools::install_github(repo = "jamespooley/motion-correction", quiet = TRUE)
-
-# analysis <- function() {
-# 
-#   library(readr)
-#   library(dplyr)
-#   library(purrr)
-#   library(ggplot2)
-#   
-#   source("functions.R")  # Read in helper functions for the analyses
-#   
-#   adhd200 <- read_csv("data/adhd200.csv")
-#   qap_func <- read_csv("data/qap_func.csv")
-#   qap_struct <- read_csv("data/qap_struct.csv")
-#   
-#   
-#   roi_list <- names(adhd200)[16:80]
-#   
-#   # Effect of Motion on Neurodevelopmental Trajectories
-#   
-#   
-#   
-#   results <- map2(roi_list, blah_blah_blah)
-#   results_df <- results %>% tbl_df
-# 
-# }
 
 qap_measures <- c("EFC", "FWHM", "Qi1", "SNR", "FBER", "CNR")
 roi_names <- names(pardoe_df)[16:80]
@@ -35,8 +12,23 @@ roi_list <- as.list(roi_names)
 map(roi_list, get_peak_age, 
     dplyr::filter(analysis_df, sex == "female", site == "NYUCSC", diagnosis == "adhd"))
 
-map_dbl(roi_list, get_peak_age, subset(analysis_df, sex == "male"))
-map_dbl(roi_list, get_peak_age, subset(analysis_df, sex == "female"))
+peak_ages <- data_frame(
+  male = map_dbl(roi_list, get_peak_age, dplyr::filter(analysis_df, sex == "male")),
+  female = map_dbl(roi_list, get_peak_age, dplyr::filter(analysis_df, sex == "female")) 
+)
+
+peak_ages %>% View
+
+peak_ages_males <- data_frame(
+  no_motion = map_dbl(roi_list, get_peak_age, dplyr::filter(analysis_df, sex == "male", diagnosis == "adhd")),
+  motion_qi1 = map_dbl(roi_list, get_peak_age, dplyr::filter(analysis_df, sex == "male", diagnosis == "adhd"), "Qi1"),
+  motion_snr = map_dbl(roi_list, get_peak_age,
+                       dplyr::filter(analysis_df, sex == "male", diagnosis == "adhd"), "SNR"),
+  motion_qap.mean.rms = map_dbl(roi_list, get_peak_age,
+                                dplyr::filter(analysis_df, sex == "male", diagnosis == "adhd"), "qap.mean.rms"),
+  motion_mean.rms = map_dbl(roi_list, get_peak_age,
+                            dplyr::filter(analysis_df, sex == "male", diagnosis == "adhd"), "mean.rms")
+)
 
 no_motion <- map_dbl(roi_list, get_peak_age, subset(analysis_df, diagnosis == "adhd"))
 motion <- map_dbl(roi_list, get_peak_age, subset(analysis_df, diagnosis == "adhd"), "qi1")
